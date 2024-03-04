c61c9c79343675071ef7fa028e598f6b1d5a311f
tomyferra <ferra.tomy@gmail.com>
2024-03-04 19:06:25 -0300

    added database changes

--- estadios/settings.py
+++ estadios/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dtr$et#hx7ilx&n_su_$%f*q$a9eltvl1d-c4(54zk=ynqkyu4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -76,19 +77,8 @@ WSGI_APPLICATION = 'estadios.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgres://tomyferra:xjPd0WsTXhl8bnJzgdIRz8Q5Qwg5zfPH@dpg-cnj333a1hbls73flo0f0-a.oregon-postgres.render.com/futboldb',
-        conn_max_age=600
-    )
-}
+database_url = os.environ.get('DATABASE_URL')
+DATABASES = {'default': dj_database_url.parse(database_url)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
