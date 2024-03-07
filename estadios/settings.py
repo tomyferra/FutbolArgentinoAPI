3d804e82c72b3b8502edad6bd05b8ba8e8125835
tomyferra <ferra.tomy@gmail.com>
2024-03-07 14:24:06 -0300

    Added Allowed hosts

--- estadios/settings.py
+++ estadios/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
